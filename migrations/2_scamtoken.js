ab6a13558095ef2bc790687f6251bcbde6d68541
Ben Smith <admin@admins-MBP.fritz.box>
2018-07-07 15:18:11 +0200

    update migration to accomidate for new ScamICO parameter

--- migrations/2_scamtoken.js
+++ migrations/2_scamtoken.js
@@ -5,7 +5,7 @@ const ScamICO = artifacts.require("scamICO");
 module.exports = async function(deployer) {
   deployer.deploy(ScamToken).then(function() {
     return deployer.deploy(Weth9).then(function() {
-      return deployer.deploy(ScamICO, Weth9.address, ScamToken.address, 1000).then(function() {
+      return deployer.deploy(ScamICO, Weth9.address, ScamToken.address, 1000, 120).then(function() {
           return ScamToken.deployed().then(function(SCMToken) {
               SCMToken.setMinter(ScamICO.address);
           })
