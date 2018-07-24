40a9c0411291a990edf5ce849f8967496f170961
Ben Smith <admin@admins-mbp-2.intern>
2018-07-24 10:14:13 +0200

    Include Ropsten Deployment through infura

--- truffle.js
+++ truffle.js
@@ -1,23 +1,19 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() {
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = "true sort disagree tail tunnel unfair beach dutch salad alcohol gossip praise";
 
 module.exports = {
-   networks: {
-       development: {
-           host: "localhost",
-           port: 8545,
-           network_id: "*" // Match any network id
-       }
-   }
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/DRr7EzSi3ijuMUDjsbBD"),
+      network_id: 3,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    }
+  }
 };
